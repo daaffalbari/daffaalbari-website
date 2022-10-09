94e2a3228c8c7ab952af01d1884eb1a15b8cd526
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2022-10-09 21:44:38 +0700

    tidur dulu

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -3,5 +3,9 @@ import React from 'react';
 type Props = {};
 
 export default function WorkExperience({}: Props) {
-  return <div>WorkExperience</div>;
+  return (
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase text-gray-500 text-2xl tracking-[20px]">Experience</h3>
+    </div>
+  );
 }
